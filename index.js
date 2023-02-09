410db4bb334c3b24af63496f63c7e1489433e908
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-02-08 18:08:57 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+const drivers = ['taco', 'burrito', 'hamburger', 'TacO', 'TAcO', 'taco', 'tac'];
+
+function findMatching(array, searchString) {
+    const nameMatcher = array.filter(driver => {        
+         return driver.toLowerCase() === searchString.toLowerCase();
+    })
+    return nameMatcher;
+}
+
+function fuzzyMatch(array, searchString) {
+    const fuzzyMatcher = array.filter(driver => {
+
+        return driver[0] ===  searchString [0]
+    })
+
+    return fuzzyMatcher
+}
+
+function matchName(obj, searchString) {
+    const nameFinder = obj.filter(driver => {
+        return driver.name === searchString        
+    })
+    return nameFinder;
+}
+
+
+console.log(fuzzyMatch(drivers, 'tac'))
+console.log(findMatching(drivers, 'taco'))
